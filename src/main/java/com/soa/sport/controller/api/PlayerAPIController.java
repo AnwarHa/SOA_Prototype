b56f4d3c161878cfc7fccccd11aa73cb97ea6e73
Blenda Kaja <blenda.kaja@student.ucll.be>
2021-12-11 15:56:21 +0100

    basketball player frontend

--- src/main/java/com/soa/sport/controller/api/PlayerAPIController.java
+++ src/main/java/com/soa/sport/controller/api/PlayerAPIController.java
@@ -22,23 +22,49 @@ public class PlayerAPIController {
     }
 
     @GetMapping
-    public List<Player> getOverviewPlayers(Model model) {
+    public String getOverviewPlayers(Model model) {
         List<Player> players = Arrays.asList(this.playerAPIService.requestAllPlayers());
         model.addAttribute("players", players);
-        return players;
+        return "api-basketballplayers";
     }
 
     @GetMapping( value = "/{id}")
-    public Player showPlayer(@PathVariable int id, Model model){
+    public Player getShowPlayer(@PathVariable int id, Model model){
         Player player = this.playerAPIService.readPlayer(id);
         model.addAttribute("player", player);
         return player;
     }
 
-    @PostMapping(value = "/new")
+    @GetMapping( value = "/filterById")
+    public String showPlayer(@RequestParam int id, Model model){
+        model.addAttribute("players",this.playerAPIService.readPlayer(id));
+        return "api-basketballplayers";
+    }
+
+    @GetMapping(value = "/new")
+    public String getNewSoccerPlayer(Model model){
+        model.addAttribute("player",new PlayerDTO());
+        return "api-basketballplayer-new";
+    }
+
+    @PostMapping(value = "/new", produces = "application/json", consumes = "application/json")
     public PlayerDTO postNewPlayer(@RequestBody Player player) {
         PlayerDTO playerDTO = createPlayerDTO(player);
         return this.playerAPIService.create(playerDTO);
+    }
+
+    @PostMapping(value = "/new")
+    public String postNewPlayer(
+            @RequestParam(name = "first_name") String first_name,
+            @RequestParam(name = "last_name") String last_name,
+            @RequestParam(name = "team") String team,
+            @RequestParam(name = "age") int age,
+            @RequestParam(name = "height") int height,
+            @RequestParam(name = "weight") int weight
+    ){
+        PlayerDTO playerDTO = new PlayerDTO(first_name, last_name, team, age, height, weight);
+        this.playerAPIService.create(playerDTO);
+        return "redirect:/sport/api/players";
     }
 
     @PutMapping(value = "/update/{id}")
@@ -47,9 +73,38 @@ public class PlayerAPIController {
         return this.playerAPIService.update(id, playerDTO);
     }
 
+    @GetMapping("update/{id}")
+    public String getUpdatePlayer(@PathVariable int id, Model model){
+        Player player = this.playerAPIService.readPlayer(id);
+        model.addAttribute("id",id);
+        model.addAttribute("player", player);
+        return "api-basketballplayer-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updatePlayer(
+            @PathVariable int id,
+            @RequestParam(name = "first_name") String first_name,
+            @RequestParam(name = "last_name") String last_name,
+            @RequestParam(name = "team") String team,
+            @RequestParam(name = "age") int age,
+            @RequestParam(name = "height") int height,
+            @RequestParam(name = "weight") int weight
+    ){
+        PlayerDTO playerDTO = new PlayerDTO(first_name, last_name, team, age, height, weight);
+        this.playerAPIService.update(id, playerDTO);
+        return "redirect:/sport/api/players/" + id;
+    }
+
     @DeleteMapping("/delete/{id}")
     public void deletePlayers(@PathVariable int id){
         this.playerAPIService.delete(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getDeletePlayer(@PathVariable int id){
+        this.playerAPIService.delete(id);
+        return "redirect:/sport/api/players";
     }
 
     public PlayerDTO createPlayerDTO(Player player){
