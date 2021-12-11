368ca85c4475e4e0fe4e8122de4c8de2f279dfb6
RobbeVanluyten <robbe.vanluyten@student.ucll.be>
2021-12-11 14:30:20 +0100

    running_races aanpassingen

--- src/main/java/com/soa/sport/controller/api/Running_racesAPIController.java
+++ src/main/java/com/soa/sport/controller/api/Running_racesAPIController.java
@@ -1,9 +1,13 @@
 package com.soa.sport.controller.api;
 
+import com.soa.sport.model.dto.CyclistDTO;
 import com.soa.sport.model.dto.Running_raceDTO;
+import com.soa.sport.model.dto.SoccerPlayerDTO;
+import com.soa.sport.model.entity.Cyclist;
 import com.soa.sport.model.entity.Running_race;
 import com.soa.sport.model.service.Running_racesAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -12,7 +16,7 @@ import java.sql.Time;
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/sport/api/running_races")
 public class Running_racesAPIController {
 
@@ -24,24 +28,51 @@ public class Running_racesAPIController {
     }
 
     @GetMapping
-    public List<Running_race> getOverviewRunning_races(Model model) {
+    public String getOverviewRunning_races(Model model) {
         List<Running_race> running_races = Arrays.asList(this.running_racesAPIService.requestAllRunning_races());
         model.addAttribute("running_races", running_races);
-        return running_races;
+        return "api-running_races";
+    }
+    @GetMapping( value = "/{id}")
+    public String getShowRunning_racer(@PathVariable int id, Model model){
+        Running_race running_race = this.running_racesAPIService.readrunning_race(id);
+        model.addAttribute("running_races", running_race);
+        return "api-running_races";
     }
 
-    @GetMapping( value = "/{id}")
-    public Running_race showRunning_racer(@PathVariable int id, Model model){
-        Running_race running_race = this.running_racesAPIService.readrunning_race(id);
-        model.addAttribute("running_race", running_race);
-        return running_race;
+    @GetMapping( value = "/filterById")
+    public String showRunning_racer(@RequestParam int id, Model model){
+        model.addAttribute("running_races", this.running_racesAPIService.readrunning_race(id));
+        return "api-running_races";
+    }
+
+    @GetMapping(value = "/new")
+    public String getNewRunning_racer(Model model){
+        model.addAttribute("running_race",new Running_raceDTO());
+        return "api-running_races-new";
+    }
+
+    @PostMapping(value = "/new", produces = "application/json", consumes = "application/json")
+    public Running_raceDTO postNewRunning_race(@RequestBody Running_race running_race) {
+        Running_raceDTO running_raceDTO = createRunning_raceDTO(running_race);
+        return this.running_racesAPIService.create(running_raceDTO);
     }
 
     @PostMapping(value = "/new")
-    public Running_raceDTO postNewRunning_race(@RequestBody Running_race running_race) {
-        Running_raceDTO running_raceDTO = createRunning_raceDTO(running_race) ;
-        return this.running_racesAPIService.create(running_raceDTO);
+    public String postNewRunning_race(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "organizer") String organizer,
+            @RequestParam(name = "location") String location,
+            @RequestParam(name = "distance") int distance,
+            @RequestParam(name = "registration_price") int registration_price,
+            @RequestParam(name = "date") String date,
+            @RequestParam(name = "starting_hour") String starting_hour
+    ){
+        Running_raceDTO running_raceDTO = new Running_raceDTO(name,organizer,location,distance,registration_price,date,starting_hour);
+        this.running_racesAPIService.create(running_raceDTO);
+        return "redirect:/sport/api/running_races";
     }
+
 
     @PutMapping(value = "/update/{id}")
     public Running_raceDTO updateRunning_race(@PathVariable int id, @RequestBody Running_race running_race) {
@@ -49,9 +80,40 @@ public class Running_racesAPIController {
         return this.running_racesAPIService.update(id, running_raceDTO);
     }
 
+    @GetMapping("update/{id}")
+    public String getUpdateRunning_race(@PathVariable int id, Model model){
+        Running_race running_race = this.running_racesAPIService.readrunning_race(id);
+        model.addAttribute("id",id);
+        model.addAttribute("running_race", running_race);
+        return "api-running_races-update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateRunning_race(
+            @PathVariable int id,
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "organizer") String organizer,
+            @RequestParam(name = "location") String location,
+            @RequestParam(name = "distance") int distance,
+            @RequestParam(name = "registration_price") int registration_price,
+            @RequestParam(name = "date") String date,
+            @RequestParam(name = "starting_hour") String starting_hour
+    ){
+        Running_raceDTO running_raceDTO = new Running_raceDTO(name,organizer,location,distance,registration_price,date,starting_hour);
+        this.running_racesAPIService.update(id,running_raceDTO);
+        return "redirect:/sport/api/running_races/" + id;
+    }
+
+
     @DeleteMapping("/delete/{id}")
     public void deleteRunning_race(@PathVariable int id){
         this.running_racesAPIService.delete(id);
+    }
+
+    @GetMapping("/delete/{id}")
+    public String getDeleteCyclist(@PathVariable int id){
+        this.running_racesAPIService.delete(id);
+        return "redirect:/sport/api/running_races";
     }
 
     public Running_raceDTO createRunning_raceDTO(Running_race running_race){
@@ -61,7 +123,7 @@ public class Running_racesAPIController {
         int distance = running_race.getDistance();
         int registration_price = running_race.getRegistration_price();
         String date = running_race.getDate();
-        Time starting_hour = running_race.getStarting_hour();
+        String starting_hour = running_race.getStarting_hour();
         return new Running_raceDTO(name, organizer, location, distance, registration_price, date, starting_hour);
     }
 }
